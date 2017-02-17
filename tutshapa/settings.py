8871acb724b23ed920d41a88a52333a326d9182a
sylvance <kerandisylvance@gmail.com>
2017-02-17 12:18:04 +0300

    Configured filters

--- tutshapa/settings.py
+++ tutshapa/settings.py
@@ -60,6 +60,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
 ROOT_URLCONF = 'tutshapa.urls'
