bfb22745062d22fa8e83b4f40346cdaea7a78188
sylvance <kerandisylvance@gmail.com>
2017-02-17 12:21:37 +0300

    Configured pagination

--- tutshapa/settings.py
+++ tutshapa/settings.py
@@ -60,7 +60,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 ROOT_URLCONF = 'tutshapa.urls'
